dcf012a04b5deb1bb1fff80ea2c6afb5707f7bea
SJ <shijie7575@foxmail.com>
2016-06-08 23:24:10 +0800

    Initial Commit

---
+++ tableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableView
+//
+//  Created by SJ on 16/6/8.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableView
+//
+//  Created by SJ on 16/6/8.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
